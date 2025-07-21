9bb5dd9e6f2bc9f35a16cf45cd2d6aef34a63c4d
hanou <hanifnouhi@gmail.com>
2025-07-21 12:36:44 +0200

    translate e2e test was added, package json and jest-e2e json files changed accordingly

---
+++ src/translate/tests/e2e/translate.e2e-spec.ts
@@ -0,0 +1,50 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { AppModule } from '../../../app.module';
+import * as request from 'supertest';
+
+describe('TranslateController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  test('should return a translated word from Azure without target language', async () => {
+    
+    const response = await request(app.getHttpServer())
+      .post('/translate')
+      .send({ text: 'bonjour' });
+
+    expect(response.status).toBe(201);
+    expect(response.body.translation).toBeDefined();
+    expect(typeof response.body.translation).toBe('string');
+    expect(response.body.translation.toLowerCase()).toBe('hello');
+  });
+
+  test.each([
+    ['bonjour', 'ja'],
+    ['book', 'fa'],
+    ['خانه', 'hi']
+  ])('should return a translated word from azure based on the target language', async (text, targetLang) => {
+
+    const response = await request(app.getHttpServer())
+      .post('/translate')
+      .send({ text, targetLang });
+
+    expect(response.status).toBe(201);
+    expect(response.body.translation).toBeDefined();
+    expect(typeof response.body.translation).toBe('string');
+    expect(response.body.translation.length).toBeGreaterThan(0);
+  });
+});
