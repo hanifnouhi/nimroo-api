ca0fb5915999b4211b4a2e27f5d8904e61ba5b53
hanou <hanifnouhi@gmail.com>
2025-07-21 11:18:10 +0200

    translate service integration was added

---
+++ src/translate/test/translate.service.integration.spec.ts
@@ -0,0 +1,40 @@
+import { Test } from '@nestjs/testing';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../app.module';
+import axios from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('TranslateController integration', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should return translated text from Azure mock', async () => {
+    mockedAxios.post.mockResolvedValueOnce({
+      data: [{ translations: [{ text: 'hello' }] }],
+    });
+
+    return request(app.getHttpServer())
+      .post('/translate')
+      .send({ text: 'bonjour', targetLang: 'en' })
+      .expect(201)
+      .expect(res => {
+        expect(res.body).toEqual({ translation: 'hello' });
+      });
+  });
+});
