9f38dd7d7586cb6681d41918ab9176ef2df1c6b1
hanou <hanifnouhi@gmail.com>
2025-07-26 13:39:11 +0200

    chore: add logger to translate sevice

--- nestjsApp/src/translate/translate.service.ts
+++ nestjsApp/src/translate/translate.service.ts
@@ -1,8 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable} from '@nestjs/common';
 import { Cache } from 'cache-manager';
 import { TranslationProvider } from './providers/translate.interface';
 import { TranslationResult } from './providers/translation-result.interface';
 import { SpellCheckService } from '../spell-check/spell-check.service';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 
 /**
  * Service responsible for text translation.
@@ -13,7 +14,8 @@ export class TranslateService {
     constructor(
       @Inject('CACHE_MANAGER') private cacheManager: Cache,
       @Inject('TranslationProvider') private provider: TranslationProvider,
-      private readonly spellCheckService: SpellCheckService
+      private readonly spellCheckService: SpellCheckService,
+      @InjectPinoLogger(TranslateService.name) private readonly logger: PinoLogger
     ) {}
 
     /**
@@ -25,6 +27,7 @@ export class TranslateService {
      * @returns {Promise<TranslationResult>} A promise resolving to the object containing Translated text and Detected language
      */
     async translate(text: string, lang: string = 'en'): Promise<TranslationResult> {
+        this.logger.debug(`Attempting to translate ${text} to the ${lang} language`);
         //Normalize text to ensure consistent cache key generation
         const normalizedText = text.trim().toLowerCase();
         const targetLang = lang.trim().toLowerCase();
@@ -33,30 +36,36 @@ export class TranslateService {
 
         // Return cached translation if available
         if (cached) {
+          this.logger.debug(`Found result in the cache for key: ${cacheKey}. Translate: "${cached.translated}", Detected Lang: "${cached.detectedLanguage}", Corrected text: "${cached.correctedText}"`);
           return cached;
         }
 
         try {
           // Call the pluggable translation provider
+          this.logger.debug(`Attempting to send translate request to translate provider with text: ${normalizedText}, targetLang ${targetLang}`);
           const result = await this.provider.translate(normalizedText, targetLang);
           
           //Cache the result for future requests if translated
           if (result.translated) {
+            this.logger.info(`${normalizedText} translated successfully to ${result.translated}`);
             try {
+              this.logger.debug(`Attempting to check the text spell after successfull translate: ${normalizedText}, targetLang ${targetLang}`);
               let correctedText = await this.spellCheckService.correct(normalizedText, result.detectedLanguage);
               correctedText = correctedText.trim().toLowerCase();
               if (correctedText && correctedText !== normalizedText) {
                 result.correctedText = correctedText;
               }
+              this.logger.info(`${normalizedText} spell checked successfully to ${correctedText}`);
             } catch (spellCheckError) {
-              console.log('Spell check API error: ', spellCheckError.message);
+              this.logger.error({ error: spellCheckError.message, stack: spellCheckError.stack }, 'Spell check API error after successful translate.');
             }
             
+            this.logger.debug(`Attempting to save the result in cache with: cacheKey ${cacheKey}, cacheValue ${result}`));
             await this.cacheManager.set(cacheKey, result);
           }            
           return result;
         } catch (error) {
-          console.error('Azure Translate API error:', error.message);
+          this.logger.error({ error: error.message, stack: error.stack }, 'Azure Translate API error during translation.');
           return {
             translated: 'Translation failed',
             detectedLanguage: 'en'
