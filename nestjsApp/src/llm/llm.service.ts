944b7c73ad80ba1551fc990ab4dbd6818426e9be
hanou <hanifnouhi@gmail.com>
2025-10-08 21:00:49 +0200

    feat(cache): update analyze text method
    Update analyze text method to first search in the cache

--- nestjsApp/src/llm/llm.service.ts
+++ nestjsApp/src/llm/llm.service.ts
@@ -2,14 +2,23 @@ import { Inject, Injectable } from '@nestjs/common';
 import { LlmAnalyzeTextDto } from './dtos/llm-analyze-text.dto';
 import { LlmAnalyzeProvider } from './providers/llm-analyze.interface';
 import { LlmAnalyzeResult } from './providers/llm-analyze-result.interface';
+import { CacheService } from '../cache/cache.service';
 
 @Injectable()
 export class LlmService {
     constructor(
-        @Inject('LlmAnalyzeProvider') private readonly provider: LlmAnalyzeProvider
+        @Inject('LlmAnalyzeProvider') private readonly provider: LlmAnalyzeProvider,
+        private readonly cacheService: CacheService
     ){}
 
     async analyzeText(text: LlmAnalyzeTextDto): Promise<LlmAnalyzeResult | undefined> {
-        return await this.provider.analyze(text);
+        const cacheKey = `llm:${text.text}`;
+        
+        return await this.cacheService.getOrSetCachedValue<LlmAnalyzeResult | undefined>(
+            cacheKey,
+            async () => {
+                return await this.provider.analyze(text);
+            }
+        );
     }
 }
